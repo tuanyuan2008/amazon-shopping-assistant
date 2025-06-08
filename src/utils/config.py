6352da2affc4958648eb8f815646dfe91ec07fc7
tuanyuan2008 <schen1337@gmail.com>
2025-06-07 23:48:28 -0400

    Headless mode is ruining my life

--- src/utils/config.py
+++ src/utils/config.py
@@ -17,7 +17,7 @@ class Config:
         self.REQUEST_DELAY_MAX = float(os.getenv('REQUEST_DELAY_MAX', '5'))
         
         # Browser Configuration
-        raw_headless_mode_env = os.getenv('HEADLESS_MODE', 'False')
+        raw_headless_mode_env = os.getenv('HEADLESS_MODE', 'True')
         self.HEADLESS_MODE = raw_headless_mode_env.lower() == 'true'
 
         self.USER_AGENT = os.getenv('USER_AGENT', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
